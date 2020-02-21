1cfc2dd4b7cc95c1beb39744b7b847d4302cd937
eomttt <hyunt0413@gmail.com>
2020-02-21 10:04:25 +0900

    Express to script

--- controller/autoplay.js
+++ controller/autoplay.js
@@ -1,11 +1,16 @@
 const puppeteer = require('puppeteer');
 
 const URL = 'https://artofjiujitsu.com/';
-const URL_PLAYING = 'https://artofjiujitsu.com/academy-library/?filters=1&categories=competition,seminars,sparring';
+const URL_PLAYING = 'https://artofjiujitsu.com/academy-library/?filters=1&categories=competition,seminars,sparring&instructors=rafael-mendes,gui-mendes&pager=1';
+
+const URL_VIDEO_LINK = 'https://artofjiujitsu.com/library/professor-gui-sparring-aoj-purple-belt-2-18-2020/';
 
 const info = require('../auth.config');
 
 const play = async () => {
+  let pageOffset = 1;
+  let videoOffset = 1;
+
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: { width : 1227, height : 1636 }
@@ -17,7 +22,7 @@ const play = async () => {
   });
 
   try {
-      const { ID, PASSWORD } = authInfo;
+      const { ID, PASSWORD } = info;
       await page.goto(URL);
       await page.waitFor(1000);
       
@@ -35,12 +40,46 @@ const play = async () => {
 
       await page.goto(URL_PLAYING);
 
+      const articleList = await page.evaluate(() => {
+        const articles = Array.from(document.querySelectorAll('#wrapper > main > #archive-plug > .container > #archive-plug-posts > .grid-item > .entry-thumbnail > a'))
+        return articles.map((article) => {
+          return article.getAttribute('href');
+        });
+      });
+
+      await page.waitFor(1000);
+
+      await page.goto(articleList[0]);
+      await page.waitForSelector("iframe");
+      const elementHandle = await page.$('div#wrapper iframe');
+      const frame = await elementHandle.contentFrame();
+      // const src = await frame.querySelector('#vzaar-media-player > .vzaar-player-wrapper > .video-js > video').getAttribute('src');
+  
+      // await page.waitForSelector('iframe');
+      // const elementHandle = await page.$('#wrapper > main > .slideshow > .mask > .slideset > .slide > .video > iframe');
+      // console.log('elementHandle', elementHandle);
+      // const frame = await elementHandle.contentFrame();
+      // await frame.waitForSelector('#vzaar-media-player > .vzaar-player-wrapper > .video-js > video');
+      // const res = await frame$('#vzaar-media-player > .vzaar-player-wrapper > .video-js > video');
+      console.log("RES", frame);
       return 'Success'
   } catch (error) {
       console.log('Error', error);
   } finally {
-      // browser.close();
+      browser.close();
   }
+};
+
+const playVide = async (link = URL_VIDEO_LINK) => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: { width : 1227, height : 1636 }
+  });
+  const page = await browser.newPage();
+
+  page.on('dialog', async dialog => {
+      await dialog.dismiss();
+  });
 };
 
 module.exports.play = play;
--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
 const autoPlayer = require('./controller/autoplay');
 
-const app = express();
-const router = express.Router();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.raw());
+// const app = express();
+// const router = express.Router();
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.raw());
 
 console.log('Process node version', process.version);
+autoPlayer.play();
+// router.get('/autoplay', () => {
+//   console.log("AAA")
+//   autoPlayer.play();
+// });
 
-router.get('/autoplay', () => {
-  console.log("AAA")
-  autoPlayer.play();
-});
+// app.use('/api', router)
 
-app.use('/api', router)
+// app.use('/', function (req, res) {
+//   res.send('Express server running.');
+// });
 
-app.use('/', function (req, res) {
-  res.send('Express server running.');
-});
-
-app.listen(8080, () => {
-  console.log('Express server is running on http://localhost:8080');
-});
+// app.listen(8080, () => {
+//   console.log('Express server is running on http://localhost:8080');
+// });
