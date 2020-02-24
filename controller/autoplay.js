8904c9d8a4126fcd3181192d9c89bd34e4185712
eomttt <hyunt0413@gmail.com>
2020-02-24 18:57:29 +0900

    Heroku test: defaultVidwPort

--- controller/autoplay.js
+++ controller/autoplay.js
@@ -9,14 +9,14 @@ const URL_VIDEO_LINK = 'https://artofjiujitsu.com/library/professor-gui-sparring
 
 const getLink = async (pageOffset, videoOffset) => {
   // launch Options
-  // defaultViewport: { width : 1227, height : 1636 },
   const browser = await puppeteer.launch({
     headless: true,
+    defaultViewport: { width : 1227, height : 1636 },
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-web-security',
-      '--disable-features=IsolateOrigins,site-per-process'
+      '--disable-features= IsolateOrigins,site-per-process'
     ]
   });
   const page = await browser.newPage();
